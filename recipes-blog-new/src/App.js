b1b1512f3b03287d0ce4b12ad54dcfbfdf86ba6c
xsol1827x <45325971+xsol1827x@users.noreply.github.com>
2021-08-06 12:34:44 +1000

    recipe search using spoonacular api
    created with material ui

---
+++ recipes-blog-new/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Input from './Components/UI/Input';
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Recipes</h1>
+      <p>Start searching to find recipies</p>
+      <Input />
+    </div>
+  );
+}
+
+export default App;
---
+++ recipes-blog-new/src/Components/UI/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Recipes</h1>
+            <p>Start searching to find recipies</p>
+        </div>
+    )
+}
+
+export default Header
